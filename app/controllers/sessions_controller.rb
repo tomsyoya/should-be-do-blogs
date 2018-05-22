17d2d33bd50c5ac2cc0854201736bf41b56c1a89
Soya Sano <soya.sano1991@gmail.com>
2018-05-22 15:06:23 +0000

    add login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,21 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
 
   def new
   end
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(password: params[:session][:password])
+
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      render :new
+      render :new, notice: "ログインに失敗しました"
     end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to sessions_path
+    redirect_to new_session_path
   end
 end
