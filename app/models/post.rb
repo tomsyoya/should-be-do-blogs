7c8f8a633b3bf8a391434068e50b3a2432f374da
Soya Sano <soyasano2180plus1@gmail.com>
2018-06-17 23:05:05 +0000

    fix indent on model files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
-    mount_uploader :image, ImageUploader
-    
-    belongs_to :user
-    has_many :comments, dependent: :destroy
-    has_many :favorites, dependent: :destroy
-    
-    validates :title, presence: true
-    validates :content, presence: true
+  mount_uploader :image, ImageUploader
+  
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
-    mount_uploader :image, ImageUploader
-    
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    has_many :favorites, dependent: :destroy
-    
-    has_many :favorite_posts, through: :favorites, source: :post
-    
-    validates :name, presence: true, length: {maximum: 30}
-    validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
-    
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }, :on => :create
-    validates :password, confirmation: true
+  mount_uploader :image, ImageUploader
+  
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  has_many :favorite_posts, through: :favorites, source: :post
+  
+  validates :name, presence: true, length: {maximum: 30}
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, :on => :create
+  validates :password, confirmation: true
 end
