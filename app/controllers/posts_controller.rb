19a1f4efdd32dbd21e89e3c2dfa5dd27cd4e5f5a
Soya Sano <soyasano2180plus1@gmail.com>
2018-06-19 11:33:07 +0000

    Fix #8 remove all comments generated by scaffold on controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,26 +2,20 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   before_action :check_logged_in
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -33,7 +27,6 @@ class PostsController < ApplicationController
      end
   end
 
-  # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
       redirect_to @post, notice: "ポスト: #{@post.title} を更新しました"
@@ -42,7 +35,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
   def destroy
     @post.destroy
     redirect_to posts_url, notice: "ポスト: #{@post.title} を削除しました"
